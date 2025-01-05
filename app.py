bcc15d5676870fe4c5df13e87b1581a2d9d4721a
yuvienaaaa <yuvienayohani22@gmail.com>
2025-01-05 21:27:43 +0800

    new ci-cd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask! Saya sedang mengerjakan tugas"
 
 if __name__ == '__main__':
     app.run(debug=True)
